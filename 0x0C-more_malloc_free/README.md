495a9ea47f053898be45326a4882ed4b7175f1c2
root <ianogithae@gmail.com>
2023-11-02 03:58:25 -0700

    whoo

---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+this file contains all the tasks that were assigned to me on the project 0x0C.C . this topic was on malloc and free 
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_calloc(unsigned int nmemb, unsigned int size);
+void *malloc_checked(unsigned int b);
+
+#endif
